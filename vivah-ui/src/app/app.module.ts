6678a3f3839c27470687f40fcec7e3a8657d7efc
Prasad <prasadshet1@gmail.com>
2017-11-21 15:08:53 +0530

    21nov

--- vivah-ui/src/app/app.module.ts
+++ vivah-ui/src/app/app.module.ts
@@ -5,17 +5,25 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LeftNavigationComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
   BrowserModule,
-  FormsModule
+  FormsModule,
+  AppRoutingModule
 ],
   providers: [],
   bootstrap: [AppComponent]
