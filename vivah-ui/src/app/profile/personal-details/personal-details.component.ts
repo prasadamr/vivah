99d63f73c9b2c3d37d5132c7f0bd7ccfa01140cb
Praveena Kumar D G <praveenkumardg23@gmail.com>
2017-12-03 10:19:36 +0530

    removed output parameter

--- vivah-ui/src/app/profile/personal-details/personal-details.component.ts
+++ vivah-ui/src/app/profile/personal-details/personal-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { PersonalDetails } from './personal-details';
 
 @Component({
@@ -8,7 +8,6 @@ import { PersonalDetails } from './personal-details';
 })
 export class PersonalDetailsComponent implements OnInit {
   @Input() model: PersonalDetails;
-  @Output() setModel = new EventEmitter<PersonalDetails>();
 
   constructor() { }
 
